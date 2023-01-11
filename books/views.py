fa6fe255bd9f047e54cf2118ef48988fd41d7530
Rodrigo Saraiva <rodrigobezerrasaraiva@gmail.com>
2023-01-11 07:40:57 -0300

    BookSerializer, BookViewSet and DRF router

--- books/views.py
+++ books/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets, permissions
+
+from books.models import Book
+from books.serializers import BookSerializer
+
+class BooksViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
