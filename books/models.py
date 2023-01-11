b9fd708018b0ced1440e70deaaecb24bf2df9510
Rodrigo Saraiva <rodrigobezerrasaraiva@gmail.com>
2023-01-11 20:17:03 -0300

    Renaming field in Book model

--- books/models.py
+++ books/models.py
@@ -5,8 +5,8 @@ from django.db import models
 
 class Book(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4)
-    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     cover = models.ImageField(upload_to='covers', null=True, blank=True)
 
     def __str__(self):
-        return f'{self.name}'
+        return f'{self.title}'
--- books/serializers.py
+++ books/serializers.py
@@ -6,4 +6,4 @@ from books.models import Book
 class BookSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Book
-        fields = ['name', 'id', 'cover']
+        fields = ['title', 'id', 'cover']
