bae8a76b16ec4525cfb0affecffab8d2c8c33a8e
Rodrigo Saraiva <rodrigobezerrasaraiva@gmail.com>
2023-01-12 08:11:00 -0300

    Linting

--- books/serializers.py
+++ books/serializers.py
@@ -1,13 +1,12 @@
 from rest_framework import serializers
 
-from books.models import Book
-
 from authors.serializers import AuthorSerializer
+from books.models import Book
 
 
 class BookSerializer(serializers.HyperlinkedModelSerializer):
     authors = AuthorSerializer(many=True)
-    
+
     class Meta:
         model = Book
         fields = ['id', 'title', 'authors', 'cover']
