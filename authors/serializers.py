e321e1b2fe2f16ecd0d69748767375e7c45ac50f
Rodrigo Saraiva <rodrigobezerrasaraiva@gmail.com>
2023-01-15 21:29:05 -0300

    Adding drf-flex-fields lib and refactoring BookSerializer and BookCopySerializer

--- authors/serializers.py
+++ authors/serializers.py
@@ -7,3 +7,6 @@ class AuthorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Author
         fields = ['url', 'id', 'name']
+
+    def display_value(self, instance):
+        return f'{instance.name}'
--- books/serializers.py
+++ books/serializers.py
@@ -1,5 +1,5 @@
+from rest_flex_fields import FlexFieldsModelSerializer
 from rest_framework import serializers
-from rest_framework.exceptions import ValidationError
 
 from authors.models import Author
 from authors.serializers import AuthorSerializer
@@ -18,8 +18,7 @@ class PublisherSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['url', 'id', 'name']
 
 
-class BookSerializer(serializers.HyperlinkedModelSerializer):
-    authors = AuthorSerializer(many=True)
+class BookSerializer(FlexFieldsModelSerializer):
     category = serializers.SlugRelatedField(
         queryset=Category.objects.all(), slug_field='name'
     )
@@ -27,103 +26,22 @@ class BookSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Book
         fields = ['url', 'id', 'title', 'authors', 'category']
-
-    def get_or_create_author_instance(self, author_data=None):
-        if author_data:
-            author_obj, created = Author.objects.get_or_create(
-                name=author_data['name']
-            )
-
-            return author_obj
-
-    def create(self, validated_data):
-        authors_data = validated_data.pop('authors')
-
-        author_instances = [
-            self.get_or_create_author_instance(author_data)
-            for author_data in authors_data
-        ]
-
-        book = Book(
-            title=validated_data.pop('title'),
-            category=validated_data.pop('category'),
-        )
-        book.save()
-
-        for author in author_instances:
-            book.authors.add(author)
-
-        return book
-
-    def update(self, object, validated_data):
-        authors_data = validated_data.pop('authors')
-
-        author_instances = [
-            self.get_or_create_author_instance(author_data)
-            for author_data in authors_data
-        ]
-
-        object.title = validated_data.pop('title')
-        object.category = validated_data.pop('category')
-
-        object.authors.set(author_instances)
-        object.save(update_fields=['title', 'category'])
-
-        return object
+        expandable_fields = {'authors': (AuthorSerializer, {'many': True})}
 
 
-class BookCopySerializer(serializers.HyperlinkedModelSerializer):
+class BookCopySerializer(FlexFieldsModelSerializer):
     publisher = serializers.SlugRelatedField(
         queryset=Publisher.objects.all(), slug_field='name'
     )
-    book_id = serializers.PrimaryKeyRelatedField(queryset=Book.objects.all())
-    book = BookSerializer(read_only=True)
 
     class Meta:
         model = BookCopy
         fields = [
             'url',
             'id',
-            'book_id',
             'book',
             'date_published',
             'publisher',
             'cover',
         ]
-
-    def create(self, validated_data):
-        book_id = validated_data.pop('book_id').pk
-
-        try:
-            book = Book.objects.get(pk=book_id)
-        except Book.DoesNotExist:
-            raise ValidationError(
-                {'detail': 'Book does not exist in database.'}
-            )
-
-        args = {key: value for key, value in validated_data.items()}
-
-        book_copy = BookCopy.objects.create(book=book, **args)
-
-        return book_copy
-
-    def update(self, object, validated_data):
-        new_book_id = validated_data.pop('book_id').pk
-
-        try:
-            new_book = Book.objects.get(pk=new_book_id)
-        except Book.DoesNotExist:
-            raise ValidationError(
-                {'detail': 'Book does not exist in database.'}
-            )
-
-        keys = []
-
-        for key, value in validated_data.items():
-            setattr(object, key, value)
-            keys.append(key)
-
-        object.book = new_book
-        object.save(update_fields=keys)
-
-        return object
+        expandable_fields = {'book': BookSerializer}
