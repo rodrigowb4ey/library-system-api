4e8ff0e09543302ebf11e0a64c6c0e70ef73694b
Rodrigo Saraiva <rodrigobezerrasaraiva@gmail.com>
2023-01-15 22:36:39 -0300

    Adding filters to views

--- authors/views.py
+++ authors/views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters as restfilters
 from rest_framework import permissions, viewsets
 
 from authors.models import Author
@@ -8,3 +10,6 @@ class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, restfilters.SearchFilter]
+    filterset_fields = ['id', 'name']
+    search_fields = ['name']
--- books/views.py
+++ books/views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters as restfilters
 from rest_framework import permissions, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -15,18 +17,27 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, restfilters.SearchFilter]
+    filterset_fields = ['id', 'name']
+    search_fields = ['name']
 
 
 class PublisherViewSet(viewsets.ModelViewSet):
     queryset = Publisher.objects.all()
     serializer_class = PublisherSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, restfilters.SearchFilter]
+    filterset_fields = ['id', 'name']
+    search_fields = ['name']
 
 
 class BooksViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, restfilters.SearchFilter]
+    filterset_fields = ['id', 'title', 'authors', 'category']
+    search_fields = ['title', 'authors__name']
 
     @action(detail=True, methods=['get'], serializer_class=BookCopySerializer)
     def copies(self, request, pk=None):
@@ -40,3 +51,7 @@ class BookCopyViewSet(viewsets.ModelViewSet):
     queryset = BookCopy.objects.all()
     serializer_class = BookCopySerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    filter_backends = [DjangoFilterBackend, restfilters.SearchFilter]
+    filterset_fields = ['id', 'book', 'date_published', 'publisher']
+    search_fields = ['book__title', 'book__authors__name']
